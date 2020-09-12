22cdd8d4f4c833ba1986c06a937c6e1c9801c62f
Erric Rapsing <erric.rapsing@nbs.systems>
2020-09-12 22:28:04 +0800

    update test stub

--- src/app/time-allocation/interface/calendar-detail.ts
+++ src/app/time-allocation/interface/calendar-detail.ts
@@ -1,7 +1,7 @@
 export interface ICalendarDetail {
+    transaction_no: any;            // primary key of data aggregation
     date: string;                   // date show in calendar metadata
     allocations: number;            // count of tasks
-
-    remarks: string;    // any
-    is_dirty: boolean;  // upon editing details
+    remarks: string;                // any
+    is_dirty: boolean;              // upon editing details
 }
